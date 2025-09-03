56f763eeb7bedbf7b8127eea6ec22dcb1d043093
dvnnya k <danilkrasavin@outlook.com>
2025-09-03 15:26:09 +0300

    v1.4

--- src/main.js
+++ src/main.js
@@ -124,8 +124,8 @@ function analyzeSalesData(data, options) {
         name: seller.name,
         revenue: +seller.revenue.toFixed(2),
         profit: +seller.profit.toFixed(2),
-        sales_count: +seller.sales_count,
-        top_products: +seller.top_products,
+        sales_count: seller.sales_count,
+        top_products: seller.top_products,
         bonus: +seller.bonus.toFixed(2)
     }));
 }
